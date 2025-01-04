9a99a17663779c808a0c731acd8363d5511ee0eb
MindfulCadet <dejameentrar101@gmai.com>
2025-01-04 14:51:53 -0400

    Se arreglaron algunos bugs y se está construyendo el hub

--- Gratos-1/Characters/RPG_character.gd
+++ Gratos-1/Characters/RPG_character.gd
@@ -1,26 +1,22 @@
 extends CharacterBody2D
 
-
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
-
 func _ready():
-	print(name)
 	set_multiplayer_authority(name.to_int())
 
 func _physics_process(delta):
 	if is_multiplayer_authority(): 
 		var directionx = Input.get_axis("ui_left", "ui_right")
 		var directiony = Input.get_axis("ui_up","ui_down")
+
 		if directionx:
-			velocity.x = directionx * SPEED
+			velocity.x = directionx*128 
 		else:
-			velocity.x = move_toward(velocity.x, 0, SPEED)
+			velocity.x = move_toward(velocity.x, 0, 128)
 			
 		if directiony:
-			velocity.y = directiony * SPEED
+			velocity.y = directiony *128
 		else:
-			velocity.y = move_toward(velocity.y, 0, SPEED)
+			velocity.y = move_toward(velocity.y, 0, 128)
 		
 		move_and_slide()
 
