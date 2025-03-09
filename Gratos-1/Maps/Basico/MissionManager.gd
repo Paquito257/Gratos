d939ae545aaca2d3e7845f79812ee21a8f55b086
Paquito257 <aanonymatusv@gmail.com>
2025-03-09 12:35:34 -0400

    mapa de basico

--- Gratos-1/Maps/Basico/MissionManager.gd
+++ Gratos-1/Maps/Basico/MissionManager.gd
@@ -2,15 +2,17 @@ extends Node2D
 
 var activo = false
 var clicks = -1
-var completados = 0
+var completados = 1
 var contador = 1
-var correcta = {"Nivel1": {"1": "6", "2": "Lineal", "3": "6"}, "Nivel2": {"1": "3cm", "2": "sisito", "3": "SanS?"}, "Nivel3": {"1":"verde", "2":"Nao", "3":"Fiora"}}
+var correcta = {"Nivel1": {"1": "6", "2": "Lineal", "3": "6"}, "Nivel2": {"1": "1990", "2": "9", "3": "1"}, "Nivel3": {"1":"8", "2":"mujer", "3":"14"}}
 var revision = {"Nivel1": {"1": false,"2":false,"3":false}, "Nivel2": {"1": false, "2": false, "3": false}, "Nivel3": {"1": false, "2": false, "3": false}}
-var preguntas = {"Nivel1": {"1": "Cuantas consonantes hay en la palabra 'palindromo'?", "2": "A que tipo función corresponde la siguiente expresión: y = mx+b?", "3": "Desde el inicio del siglo XXI cuantos años bisiestos ha habido?"}, "Nivel2": {"1":"Mide el universo?", "2": "pollito?", "3": "Papyrus?"}, "Nivel3": {"1": "azul o verde?", "2": "sis?", "3": "mejor campeon de lol?"}}
+var preguntas = {"Nivel1": {"1": "Cuantas consonantes hay en la palabra 'palindromo'?", "2": "A que tipo función corresponde la siguiente expresión: y = mx+b?", "3": "Desde el inicio del siglo XXI cuantos años bisiestos ha habido?"}, "Nivel2": {"1":"En que año salió la PS1?", "2": "Cuantas generaciones hay actualmente de pokemon (2025)", "3": "A que es igual 0!"}, "Nivel3": {"1": "En mario 64, cuantas estrellas se pueden conseguir en cada nivel?", "2": "En la saga Metroid, la persona que se encuentra dentro de la armadura de Samus es un hombre o una mujer?", "3": "En el LoL (League of Legends) cuantos campeones pertenecientes a la raza 'yordle' hay?"}}
 var pesca = false
 var cajas = false
 var respuesta = ""
 var Trivia = false
+var areaP = false
+var dentro = false
 
 var misiones = {
 	"1": {"nombre": "Pesca", "descripcion": "Ayuda al aventurero a pescar para alimentar a los gatos", "completada": false},
@@ -23,31 +25,47 @@ var misiones = {
 func _ready():
 	$"../miniBoss".visible = false
 	$"../respuesta".visible = false
-
+	$"../michicaja".play("Box")
+	$"../michi".play("Idle")
 #Revisa constantemente si se entró a las areas de cada mision
 #en caso de que se haya entrado y se presione la tecla correspondiente inicia el minijuego
 func _process(delta):
-	if $"../NPCPesca".inside and Input.is_action_just_pressed("ui_accept") and !pesca:
-		print("me ayudas a pescar?")
+	if $"../NPCPesca".inside and Input.is_action_just_pressed("ui_cancel") and !pesca:
 		pesca = true
 		clicks = 0
-	if $"../Area2D".area_entered and Input.is_action_just_pressed("ui_cancel") and pesca:
+	if areaP and Input.is_action_just_pressed("ui_accept") and pesca:
 		if clicks == 0:
 			pescar(completados)
 	if activo and pesca:
 		if Input.is_action_just_pressed("ui_accept"):
 			clicks += 1
 			print(clicks)
-	'''if $"../NPCCajas".inside and Input.is_action_just_pressed("ui_cancel") and misiones["1"]["completada"] == true:
-		if misiones["2"]["completada"] != true:
+	if $"../NPCCajas".inside and Input.is_action_just_pressed("ui_cancel"):
+		if !misiones["2"]["completada"]:
 			cajas = true
-	if $"../Caja".area_entered and Input.is_action_just_pressed("ui_accept") and cajas and misiones["1"]["completada"] == true:
+	if Input.is_action_just_pressed("ui_accept") and cajas and dentro:
 		print("encontraste la caja correcta")
 		busqueda()
-	if $"../NPCTrivia".inside and Input.is_action_just_pressed("ui_cancel") and misiones["2"]["completada"] == true:
+	if $"../NPCTrivia".inside and Input.is_action_just_pressed("ui_cancel"):
 		$"../respuesta".visible = true
 		Trivia = true
-		trivia()'''
+	if Trivia:
+		var pregunta = preguntas["Nivel"+str(contador)][str(completados)]
+		$"../respuesta".placeholder_text = pregunta
+		var bien
+		if Input.is_action_just_pressed("ui_cancel") and $"../respuesta".visible == true:
+			respuesta = $"../respuesta".text
+			bien = responder(preguntas, respuesta, correcta["Nivel"+str(contador)][str(completados)])
+		
+		if bien == true:
+			revision["Nivel"+str(contador)][str(completados)] = true
+			completados += 1
+			bien = false
+		if completados > 3 and contador < 3:
+			completados = 1
+			contador += 1
+		elif completados > 3 and contador == 3:
+			trivia()
 
 
 func pescar(completada):
@@ -67,6 +85,7 @@ func pescar(completada):
 	else:
 		print("Felicidades, pescaste suficiente comida para los gatos, ahora ve con el X para entregarla")
 		misiones["1"]["completada"] = true
+		completados = 1
 	
 
 func busqueda():
@@ -75,31 +94,12 @@ func busqueda():
 	cajas = false
 
 
-#solo falta comprobar que funcione igual al integrarlo
 func trivia():
 	if revision["Nivel3"]["3"] == true:
 		misiones["3"]["completada"] = true
 		Trivia = false
 		print(misiones)
-	else:
-		var pregunta = preguntas["Nivel"+str(contador)][str(completados)]
-		var bien
 	
-		if Input.is_action_just_pressed("ui_accept") and $respuesta.visible == true:
-			respuesta = $"../respuesta".text
-			print(respuesta)
-			bien = responder(preguntas, respuesta, correcta["Nivel"+str(contador)][str(completados)])
-		
-		if bien == true:
-			revision["Nivel"+str(contador)][str(completados)] = true
-			completados += 1
-			bien = false
-			print(revision)
-			print(preguntas)
-		if completados > 3 and contador < 3:
-			completados = 1
-			contador += 1
-		
 
 func responder(pregunta, respuesta, correcta):
 	if respuesta == correcta:
@@ -134,18 +134,26 @@ func _on_timer_timeout():
 		print("perdiste")
 		clicks = 0
 	#Nivel 2: Medio
-	elif clicks >= 40 and completados == 1:
+	elif clicks >= 35 and completados == 1:
 		print("ganaste")
 		completados += 1
 		clicks = 0
-	elif clicks < 40 and completados == 1:
+	elif clicks < 35 and completados == 1:
 		print("perdiste")
 		clicks = 0
 	#Nivel 3: Dificil
-	elif clicks >= 45 and completados == 2:
+	elif clicks >= 36 and completados == 2:
 		print("ganaste el minijuego completo")
 		completados += 1
 		pescar(completados)
-	elif clicks < 45 and completados == 2:
+	elif clicks < 36 and completados == 2:
 		print("perdiste")
 		clicks = 0
+
+
+func _on_area_2d_area_entered(area: Area2D):
+	areaP = true
+
+
+func _on_caja_area_entered(area: Area2D):
+	dentro = true
